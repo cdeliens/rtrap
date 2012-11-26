cd81adbb4eb00a6869549ddcc94321231c4562a0
cdeliens <cdeliens@gmail.com>
2012-11-25 18:26:07 -0600

    Initial Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtrap::Application.config.secret_token = '4078094dbcabf04f5588f0236d9acdd4418586751342c04c20b657fcc2dc4626fb2e2e824bfb273d61fa0285117df7ea4c428d18ba06a6e106dd04bcdd564fbb'
