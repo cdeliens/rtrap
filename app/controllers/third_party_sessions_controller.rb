0ed54897f608b8d47fd7b5c39059c50567cba746
cdeliens <cdeliens@gmail.com>
2013-02-12 21:47:34 -0600

    2nd step after login fix

--- app/controllers/third_party_sessions_controller.rb
+++ app/controllers/third_party_sessions_controller.rb
@@ -4,21 +4,24 @@ class ThirdPartySessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
+    
     if session[:user_id]
       user = User.find(session[:user_id])
       user.add_provider(auth_hash)
       session[:username] = user.email
     else
       auth = Authorization.find_or_create(auth_hash)
-      if auth
+      user = User.find_by_id auth.user.id if auth
+
+      if user && user.profile.complete?
         user = User.find_by_id auth.user.id
         session[:user_id] = user.id
         session[:username] = user.email
         redirect_to profile_path(user.profile)
       else
         redirect_to after_signup_path(:complete_profile, profile: auth.user.profile)
-      end
-      
+      end  
+    
     end
   end
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,13 @@
 class Profile < ActiveRecord::Base
-  attr_accessible :email, :full_name, :gender, :image, :location, :user, :address
+  attr_accessible :email, :full_name, :gender, :image, :location, :user, :address, :user_id
   belongs_to :user
+
+  def complete?
+    status = true
+    status = false if  self.email.blank?  || (/@missing.com/.match(self.email))
+    status = false  if self.address.blank?
+    status = false  if self.gender.blank?
+    status
+  end
+  
 end
