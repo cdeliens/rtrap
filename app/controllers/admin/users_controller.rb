cc9f257ddc173637e6f3da875bcf050ff6343530
cdeliens <cdeliens@gmail.com>
2013-02-23 17:55:16 -0600

    Admin for Users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,22 @@
 class Admin::UsersController < InheritedResources::Base
     layout "admin/application"
+    def new
+      @user = User.new
+    end
+
     def index
       @users_grid = initialize_grid(User)
+    end
+
+    def edit
+      @user = User.find params[:id]
+    end
+    def show
+      @user = User.find params[:id]
+    end
+    def update
+      @user = User.find params[:id]
+      @user.update_attributes params[:user]
+      redirect_to admin_users_path
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible  :username, :password, :password_confirmation, :remember_me, :email
+  attr_accessible  :username, :password, :password_confirmation, :remember_me, :email, :profile, :profile_attributes
   has_many :authorizations, :dependent => :destroy
+  accepts_nested_attributes_for :profile
 
   def add_provider(auth_hash)
     unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
