4c1693a858b116d8548c30a083a8a4db73c5d255
Beatriz Gonzalez <beatgonza@gmail.com>
2013-02-24 01:30:02 -0600

    Fixes on current user from fb and twitter

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -1,13 +1,26 @@
 class AfterSignupController < ApplicationController
+  
+  before_filter :set_current_user
+
   include Wicked::Wizard
   steps :complete_profile
-  def show
-    current_user = User.find(params[:user_id]) if params[:user_id] && current_user.blank?
 
+  def show
     @profile = Profile.find_by_id(params[:profile]) || Profile.create!(user: current_user)
     case step
     when :complete_profile
     end
     render_wizard
+  end
+
+  private
+
+  def set_current_user
+    if params[:user_id] && current_user.blank?
+      @current_user = User.find(params[:user_id]) 
+    elsif session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+    sign_in(@current_user)
   end
 end
